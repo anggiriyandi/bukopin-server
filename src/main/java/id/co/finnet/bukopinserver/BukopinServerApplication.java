d0c2ebb3a2b06d6ead95e3f77e8d616ace841f4b
Anggi Riyandi <anggi.riyandi@gmail.com>
2018-12-14 14:22:03 +0700

    setup jpos server

---
+++ src/main/java/id/co/finnet/bukopinserver/BukopinServerApplication.java
@@ -0,0 +1,18 @@
+package id.co.finnet.bukopinserver;
+
+import org.jpos.q2.Q2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BukopinServerApplication {
+
+	public static void main(String[] args) {
+                Q2 q2 = new Q2();
+                q2.start();
+                
+		SpringApplication.run(BukopinServerApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/id/co/finnet/bukopinserver/iso8583/MessageHandler.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.co.finnet.bukopinserver.iso8583;
+
+import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISORequestListener;
+import org.jpos.iso.ISOSource;
+
+/**
+ *
+ * @author anggi
+ */
+
+public class MessageHandler implements ISORequestListener{
+
+    @Override
+    public boolean process(ISOSource isos, ISOMsg isomsg) {
+        
+        
+        return true;
+        
+    }
+    
+}
